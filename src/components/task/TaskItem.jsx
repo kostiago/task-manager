2679bc4dac2719f680685b974563ecc27fd8ddec
kostiago <tiago_pb-@hotmail.com>
2024-01-16 10:03:27 -0300

    feat: update TaskItem component

--- src/components/task/TaskItem.jsx
+++ src/components/task/TaskItem.jsx
@@ -1,9 +1,30 @@
+import "./TaskItem.scss";
+
+import { MdDeleteForever } from "react-icons/md";
+
 const TaskItem = ({ task }) => {
   return (
-    <>
-      <h1>{task.description}</h1>
-      <p>{task.isCompleted ? "Completa" : "Pendente"}</p>
-    </>
+    <section className="task-item-container">
+      <div className="task-description">
+        <label
+          className={
+            task.isCompleted
+              ? "checkbox-container-completed"
+              : "checkbox-container"
+          }
+        >
+          {task.description}
+          <input type="checkbox" defaultChecked={task.isCompleted} />
+          <span
+            className={task.isCompleted ? "checkmark completed" : "checkmark"}
+          ></span>
+        </label>
+      </div>
+
+      <div className="delete">
+        <MdDeleteForever size={18} color="#f97474" />
+      </div>
+    </section>
   );
 };
 
